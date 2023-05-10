308a00ecb0493121c1269b9c5c036226b8de5c96
unknown <bleach_uekiza99@hotmail.com>
2023-05-10 22:35:09 +0700

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import React, { useEffect, useState, useReducer } from 'react'
-import LoginArea from './LoginArea'
-
-function reducer(state, action) {
-  if (action.type === 'login') {
-    const { username, fullname } = action.payload
-    return { username, fullname}
-  }
-  if (action.type === 'logout') {
-    return null
-  }
-  return state;
-}
-
-const AuthContext = React.createContext()
+import React, { useEffect, useState, useReducer, useRef } from 'react'
 
 function App() {
-  const [ authState, authDispatch ] = useReducer(reducer, null)
-  // const [ auth, setAuth ] = useState(null)
+  const fileRef = useRef(null)
+  const textareaRef = useRef(null)
+
+  function onAllTextClick() {
+    textareaRef.current.focus()
+    textareaRef.current.select()
+  }
+  
+  function onUploadClick() {
+    const myFIle = fileRef.current.files[0]
+    console.log(myFIle);
+  }
+
   return (
-    <AuthContext.Provider value={{ authState, authDispatch }}>
       <section className='app-section'>
-        <div className='app-container' />
-        <LoginArea />
+        <div className='app-container'>
+          <h2>ไม่ใช่แฟนทำแทนก็ได้</h2>
+          <p>
+            <textarea ref={textareaRef}/>
+          </p>
+          <p>
+            <button onClick={onAllTextClick}>เลือกข้อความทั้งหมด</button>
+          </p>
+          <p>
+            <input type='file' ref={fileRef} />
+          </p>
+          <p>
+            <button onClick={onUploadClick}>อัพไฟล์</button>
+          </p>
+        </div>
       </section>
-    </AuthContext.Provider>
   )
 }
 
-export { AuthContext }
 export default App
